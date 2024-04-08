dd89fab2770e0ce6e590b482790838206e6cc545
Gunski <159125111+Thaumagraph@users.noreply.github.com>
2024-04-08 13:47:34 +0200

    Styling

--- assets/js/components/busPlan.js
+++ assets/js/components/busPlan.js
@@ -5,6 +5,7 @@ import { myFetch } from "../utils/apiUtils.js";
  */
 export const busPlan = async () => {
   const container = document.getElementById("busPlan");
+  container.innerHTML = "";
 
   const endpoint =
     "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1";
@@ -19,8 +20,8 @@ export const busPlan = async () => {
   const liTime = document.createElement("li");
   liTime.innerText = "Tid";
 
-  ul.append(liLine, liStop, liTime)
-  container.append(ul)
+  ul.append(liLine, liStop, liTime);
+  container.append(ul);
 
   /**
    * LOOP
@@ -37,11 +38,44 @@ export const busPlan = async () => {
       liStop.innerText = value.stop;
 
       const liTime = document.createElement("li");
-      liTime.innerText = value.time;
+      liTime.innerText = calcRemainingTime(`${value.date} ${value.time}`);
 
       ul.append(liLine, liStop, liTime);
-      console.log(value.name, index);
+      //   console.log(value.name, index);
       container.append(ul);
     });
   }
+  setTimeout(busPlan, 3600);
+};
+
+const calcRemainingTime = (departureTime) => {
+  const currentTimeStamp = new Date().getTime();
+  //   console.log(currentTimeStamp);
+
+  const arrayDepartureTime = departureTime.split(/[.: ]/);
+
+  const departureYear = new Date().getFullYear();
+  const departureMonth = parseInt(arrayDepartureTime[1], 10) - 1;
+  const departureDay = parseInt(arrayDepartureTime[0]);
+  const departureHours = parseInt(arrayDepartureTime[3]);
+  const departureMinutes = parseInt(arrayDepartureTime[4]);
+
+  const departureTimeStamp = new Date(
+    departureYear,
+    departureMonth,
+    departureDay,
+    departureHours,
+    departureMinutes
+  ).getTime();
+
+  console.log(new Date(departureTimeStamp));
+
+  const differenceSeconds = Math.abs(
+    Math.floor((departureTimeStamp - currentTimeStamp) / 1000)
+  );
+  const differenceHours = Math.floor(differenceSeconds / 3600);
+  const differenceMinutes = Math.floor(differenceSeconds / 60);
+  return differenceHours
+    ? `${differenceHours} t ${differenceMinutes} m`
+    : `${differenceMinutes} m`;
 };
